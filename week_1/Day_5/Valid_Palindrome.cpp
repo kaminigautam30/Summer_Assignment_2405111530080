34b8d5fa10c7a41a2c88d62328aaa2901554b9eb
kaminigautam30 <gautamkamini32@gmail.com>
2026-06-05 23:34:45 +0530

    Create Valid_Palindrome.cpp

---
+++ week_1/Day_5/Valid_Palindrome.cpp
@@ -0,0 +1,22 @@
+lass Solution {
+public:
+    bool isPalindrome(string s) {
+        string str = "" ;
+        for (char ch : s ) {
+            if (isalnum (ch)) {
+                str += tolower (ch) ;
+            }
+        }
+        int i = 0 ;
+        int j = str.size() - 1 ;
+
+        while (i < j) {
+            if (str [i] != str [j]) {
+                return false ;
+            }
+            i++ ;
+            j--;        
+        }
+        return true ;
+    }   
+};
