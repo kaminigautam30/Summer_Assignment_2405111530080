eb4e983d3c535fc5b630470dfb790a313c27c9b1
kaminigautam30 <gautamkamini32@gmail.com>
2026-06-20 22:21:44 +0530

    Create Middle_of_the-Linked_List.cpp

---
+++ Week_3/Day_1/Middle_of_the-Linked_List.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* ptr1 = head;
+        ListNode* ptr2 = head;
+
+        while (ptr2 != NULL && ptr2 -> next != NULL) {
+            ptr1 = ptr1 -> next;
+            ptr2 = ptr2 -> next -> next;
+        }
+        return ptr1; 
+    }
+};
