ce3c12af2b6f581b384993172c7c53e937349fd8
kaminigautam30 <gautamkamini32@gmail.com>
2026-06-21 01:45:24 +0530

    Create oops_solution_3.java

---
+++ Week_3/oops_solution_3.java
@@ -0,0 +1,60 @@
+package oops_solution_3;
+import java.util.Random;
+
+abstract class Compartment {
+   public abstract String notice();
+}
+
+class FirstClass extends Compartment {
+    public String notice() {
+        return "First Class: Comfortable travel.";
+    }
+}
+
+class Ladies extends Compartment {
+     public String notice() {
+        return "Ladies Compartment: Reserved for women.";
+    }
+}
+
+class General extends Compartment {
+      public String notice() {
+        return "General Compartment: Open for all passenegers.";
+    }
+}
+
+class Luggage extends Compartment {
+     public String notice() {
+        return "Luggage Compartment: Goods only.";
+    } 
+}
+
+public class Main {
+    public static void main (String[] args) {
+        Compartment[] compartments = new Compartment[10];
+        Random rand = new Random();
+        
+        for (int i = 0; i < 10; i++) {
+            int choice = rand.nextInt(4) +1;
+            
+            switch(choice) {
+                case 1:
+                    compartments[i] = new FirstClass();
+                    break;
+                case 2:
+                    compartments[i] = new Ladies();
+                    break;
+                case 3:
+                    compartments[i] = new General();
+                    break;
+                case 4:
+                    compartments[i] = new Luggage();
+                    break;
+            }
+        }
+        
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Compartment" + (i + 1) + ":" + compartments[i].notice());
+        }
+    }
+}
