5c037be1d7b90d29afe1348902ad44923780c7b0
kaminigautam30 <gautamkamini32@gmail.com>
2026-06-21 11:08:07 +0530

    Create Longest_Substring_without_repeating_characters.cpp

---
+++ Week_2/Day_2/Longest_Substring_without_repeating_characters.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+       unordered_map <char,int> mp;
+       int left = 0, maxlen = 0;
+
+       for (int right = 0; right <s.size(); right++) {
+        if(mp.count(s[right]) && mp[s[right]] >= left) {
+            left = mp[s[right]] + 1;
+        }
+        mp[s[right]] = right;
+        maxlen = max(maxlen, right - left + 1);
+       }
+       return maxlen; 
+    }
+};
