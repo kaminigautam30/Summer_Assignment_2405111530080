dba852d5205c1e949d48c1ef3e4d208f7a1f245b
kaminigautam30 <gautamkamini32@gmail.com>
2026-06-22 18:50:13 +0530

    Create Maximum_Depth_of_Binary_Tree.cpp

---
+++ Week_4/Day_1/Maximum_Depth_of_Binary_Tree.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == NULL) return 0;
+
+        stack<pair<TreeNode* ,int>> st;
+        st.push({root , 1});
+
+        int maxDepth = 0;
+
+        while (!st.empty()) {
+            auto [node, depth] = st.top();
+            st.pop();
+
+            if( node != NULL) {
+                maxDepth = max(maxDepth , depth);
+
+                if (node -> left)
+                    st.push({node -> left, depth + 1});
+
+                if (node -> right)
+                    st.push({node -> right, depth + 1});
+            }
+        }
+        return maxDepth;
+    }
+};
